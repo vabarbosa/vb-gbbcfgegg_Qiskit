c9f1e675234f408cd09349ce3cde52daf42ba819
Alberto-Alive <alberto-alive@outlook.com>
2023-03-08 13:02:06 +0000

    Add files

---
+++ The Atoms of Computation.py
@@ -0,0 +1,21 @@
+#--------Your first quantum circuit
+
+#Install Qiskit
+from qiskit import QuantumCircuit, Aer
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+
+
+# Create the circuit
+qc_output = QuantumCircuit(8)
+qc_output.measure_all()
+qc_output.draw(initial_state=True)
+
+
+sim = Aer.get_backend('aer_simulator')
+result = sim.run(qc_output).result()
+counts = result.get_counts()
+plot_histogram(counts)
+
+# Save the histogram to a PNG file
+plt.savefig('histogram.png')
